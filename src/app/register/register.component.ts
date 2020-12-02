c614d23e6fab1f79754ff9838ad3fbda2d158415
Martin <martin96naumoski@hotmail.com>
2020-12-02 15:28:09 +0100

    validations

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -14,15 +14,35 @@ export class RegisterComponent implements OnInit {
     password_confirmation: ''
   }
   error = '';
+  succes = '';
   constructor(private loginService: LoginService) { }
 
   ngOnInit(): void {
   }
 
   registerUser() {
-    this.loginService.registerUser(this.user).subscribe(data => {}, error => {
-      this.error = error.error.message;
-    });
+    console.log(this.validateUser(this.user))
+    
+    if( this.validateUser(this.user) ){
+      this.loginService.registerUser(this.user).subscribe(data => {
+        this.succes = "You created new user."
+      }, error => {
+        this.error = error.error.message;
+      });
+    }
+    
+  }
+  validateUser(user: any) {
+    this.error = '';
+    let valFlag: boolean = false;
+    user.name == '' ? this.error += "Must enter name . " : '';
+    !/^[^@]+@[^@]{2,}\.[^@]{2,}$/.test(user.email) ? this.error += 'Must enter valid email. ' : '';
+    user.password == '' ? this.error += 'Must enter password.' : '';
+    user.password != user.password_confirmation ? this.error += 'Two passwords must be the same.' : '';
+    if( user.name != '' &&  /^[^@]+@[^@]{2,}\.[^@]{2,}$/.test(user.email) && user.password != '' && user.password == user.password_confirmation){
+      valFlag = true;
+    }
+    return valFlag;
   }
 
 }
