26b80aded587bbf17ef27217ddf492952f091983
Martin <martin96naumoski@hotmail.com>
2020-12-09 17:26:06 +0100

    m

--- src/app/detail-property/detail-property.component.ts
+++ src/app/detail-property/detail-property.component.ts
@@ -14,8 +14,8 @@ export class DetailPropertyComponent implements OnInit {
   id = 0;
   article:any = [];
   relatedArticles:any = [];
+  pictureUrl:any = '';
   constructor(private adminService: AdminServiceService,private route: ActivatedRoute,private router: Router) { }
-
   ngOnInit(): void {
     this.takeArticleId();
     this.takeRelatedArticles();
@@ -37,7 +37,7 @@ export class DetailPropertyComponent implements OnInit {
     }); 
     this.adminService.getArticle(this.id).subscribe(data => {
       this.article = data.Article;
-      console.log(this.article)
+      // this.pictureUrl = 'realestate-task.draft2017.com/storage//photos/' + this.article.photo[0].photo;
     }, error => {
     })
   }
